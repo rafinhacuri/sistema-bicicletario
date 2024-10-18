b81216db1cecc424e9fd6bc07cc2c0e1daff620a
Rafael Curi <rafinhacurig@gmail.com>
2024-10-17 21:40:15 -0300

    ✨ ajuste de middlewere

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -9,4 +9,10 @@ export default defineNuxtRouteMiddleware(async to => {
 
   // * Logado tentando acessar login
   if(loggedIn.value && to.fullPath === '/adm' && user.value?.level !== 'Administrador') return navigateTo('/')
+
+  // * nao Logado tentando acessar aluguel
+  if(!loggedIn.value && to.fullPath === '/alugue') return navigateTo('/login')
+
+  // * nao Logado tentando acessar adm
+  if(!loggedIn.value && to.fullPath === '/adm') return navigateTo('/login')
 })
